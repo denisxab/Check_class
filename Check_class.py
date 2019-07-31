b0140b838cefec5e0ade076f50a6029a1ce75d68
denisxab <47678176+denisxab@users.noreply.github.com>
2019-08-01 00:45:38 +0300

    Add files via upload

--- Check_class.py
+++ Check_class.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
 # pylint: disable=W0105
+# pylint: disable=C0303
+
 """
 Декоратор для проверки типов входящих переменных
 """
@@ -17,11 +19,15 @@ def Check_class(Reg=0):
     """
     def actual_decorator(func):
         def decorator_function(*Items, **Dicts):
-            # 0.008 Длительность
-            
+            """
+            Комбенированная проврека
+            0.008 Длительность проверки
+            """
+
             if Reg:
                 print('___________________________________')
                 print(f'Func_Name: {func.__name__}')
+
                 if Dicts:
                     print('**Kargs: {}'.format([[x, type(x)]
                                                 for x in Dicts.values()]))
@@ -32,20 +38,16 @@ def Check_class(Reg=0):
             if value_function.get('return'):  # отчистка от ->
                 value_function.pop('return')
 
-            # Комбенированная проврека
-
+            # Если значения переданы без присваивания - fanc(login,password_VK)
             if Items:
-                # Если значения переданы без присваивания
-                # fanc(login,password_VK)
                 test_Gen_Items = [x for x in zip(
                     value_function.keys(), Items, value_function.values())]
                 for x in test_Gen_Items:
                     if not isinstance(x[1], x[2]):
                         raise Exception(f'\n\n*Args-| {x[0]} != {x[2]}\n')
 
+            # Если присваиваться значение - fanc(passwords=password_VK)
             if Dicts:
-                # Если присваиваться значение
-                # fanc(passwords=password_VK)
                 test_Gen_Dicts = [list(x)+[value_function.get(x[0])]
                                   for x in zip(Dicts.keys(), Dicts.values())]
                 for x in test_Gen_Dicts:
@@ -63,52 +65,5 @@ def Check_class(Reg=0):
     return actual_decorator
 
 
-"""
-
-# print(iters) # Значения переданные в Декоратор !
-# print(func) # Основная функций после декоратора
-# print(Items) # Значения переданные в Функцию ! => tuple
-# print(Dicts) # Значения переданные в Функцию ! => dict
-# print(*Dicts) # Значения переданные в Функцию ! => dict
-
-    #     print(dir(func))
-    #     print(func.__annotations__)# описание def Audio_VK(vk_sessions:vk_api.vk_api.VkApi):
-    #     #print(func.__name__)# имя функции
-    #     #print(func.__kwdefaults__)# все стандартные значения  функции
-    #     #print(func.__globals__)#  Словарь, определяющий глобальное пространство имен
-    #     #print(dir(func.__eq__))#?
-    #     #print(func.__doc__)# Строка документирования
-    #     #print(func.__dict__)# Словарь, содержащий атрибуты функции
-    #     #print(dir(func.__delattr__))# ?
-    #     #print(func.__defaults__) # Кортеж с аргументами по умолчанию
-    #     #print(dir(func.__code__))# не надо
-    #     #print(func.__class__)# прсто класс
-    #     #print(dir(func.__call__)) # ?
-
-Декоратор для проверки типов входящих переменных
-
-- Состояния покоя : ничего не делает
-@Check_class({}) 
-
-
-- Работа : указать словарем неограниченное количество элементов 
-и правильных для них классов
-
-{'название переменной в вызываемой функции' : правильный класс}
-
-@Check_class({'arr_str':int})
-
-def Fanc(arr_str, int_s):
-    return "ВЫПОЛЕННО"
-
-print(Fanc(arr_str='123',int_s='s1'))
-
-# (False, {'Error Type': ('arr_str', '123', <class 'int'>)})
-
-
-Проверяет:
-- Тип переменной с заданной в декораторе 
-- Имя пременной преданное в декаратор на налиеие этого имяни в 
-функции 
-
-"""
+if __name__ == '__main__':
+    Check_class()
