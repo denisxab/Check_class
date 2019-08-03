c7d3fa5ff5619812a41e6934fc103997d9b4ab40
denisxab <47678176+denisxab@users.noreply.github.com>
2019-08-03 21:01:10 +0300

    Add files via upload

--- Check_class.py
+++ Check_class.py
@@ -2,39 +2,69 @@
 # pylint: disable=C0103
 # pylint: disable=W0105
 # pylint: disable=C0303
+"""
+from Check_class import Check_class
+@Check_class(1) # None пропустить элемент
+def Entrance_VK(logins:str, li:None,passwords:None)-> True:
+    pass
 
+Entrance_VK(123,[],'12312')
 """
-Декоратор для проверки типов входящих переменных
-"""
+
+
 import time
 import sys
 
-"""
-from Check_class import Check_class
-@Check_class(1) 
-def Entrance_VK(logins:str, passwords:str)-> True:
-    pass
-Entrance_VK(123,'12312')
-"""
 
 def Check_class(Reg=0):
+    """
+    Декоратор для проверки типов входящих переменных
+    """
     def actual_decorator(func):
         def decorator_function(*Items, **Dicts):
             """
             Комбенированная проврека
-            0.008 Длительность проверки
+            0.003 Длительность проверки
             """
 
-            if Reg:
-                print('___________________________________')
-                print(f'Func_Name: {func.__name__}')
+            def statistics(reg):
+                if reg == 0:
+                    print('______________________________________________________________________')
+                    print(f'Name: {func.__name__}')
 
-                if Dicts:
-                    print('**Kargs: {}'.format(['{0}|=|{1}|=|{2}-Байт'.format(x, type(x), sys.getsizeof(x))
-                                                for x in Dicts.values()]))
-                if Items:
+                    if Dicts:
+                        print('**Kargs:')
+                        for x in enumerate(['{0} |=| {1} |=|{2}Бт'.format(''.join(list(str(x).ljust(35))[:35].ljust(10)),
+                                                                          type(
+                                x).__name__.ljust(7),
+                                sys.getsizeof(x)) for x in Dicts.values()]):
+                            print(f'{x[0]}=> : {x[1]}')
+
+                    if Items:
+                        print('*Arg:')
+                        for x in enumerate(['{0} |=| {1} |=|{2}Бт'.format(''.join(list(str(x).ljust(35))[:35]),
+                                                                          type(
+                                x).__name__.ljust(7),
+                                sys.getsizeof(x)) for x in Items]):
+                            print(f'{x[0]}=> : {x[1]}')
+
+                if reg == 1:
+                    start = time.time()
+                    func_Time = func(*Items, **Dicts)
+                    print('>>> : {0} |=| {1} |=|{2}Бт'.format(''.join(list(str(func_Time).ljust(35))[
+                          :35]), type(func_Time).__name__.ljust(7), sys.getsizeof(func_Time)))
                     print(
-                        '*Arg: {}'.format(['{0}|=|{1}|=|{2}-Байт'.format(x, type(x), sys.getsizeof(x)) for x in Items]))
+                        'Time: {1} |=| {0}сек.'.format(time.time()-start, func.__name__.ljust(35)))
+                    print('----------------------------------------------------------------------')
+
+                    return func_Time
+
+            if Reg:
+                statistics(0)
+
+            if func.__class__.__name__ == 'type':
+                func_Time = statistics(1)
+                return func_Time
 
             value_function = func.__annotations__
             if value_function.get('return'):  # отчистка от ->
@@ -45,8 +75,11 @@ def Check_class(Reg=0):
                 test_Gen_Items = [x for x in zip(
                     value_function.keys(), Items, value_function.values())]
                 for x in test_Gen_Items:
+                    if x[2] == None:  # pylint: disable=C0121
+                        continue
                     if not isinstance(x[1], x[2]):
-                        raise Exception(f'\n\n*Args-| {x[0]} != {x[2]}\n')
+                        print(x[1], x[2])
+                        raise Exception(f'\n\n{func.__name__} *Args-| {x[0]} != {x[2].__name__}\n')
 
             # Если присваиваться значение - fanc(passwords=password_VK)
             if Dicts:
@@ -54,13 +87,10 @@ def Check_class(Reg=0):
                                   for x in zip(Dicts.keys(), Dicts.values())]
                 for x in test_Gen_Dicts:
                     if not isinstance(x[1], x[2]):
-                        raise Exception(f'\n\n**Kargs-| {x[0]} != {x[2]}\n')
+                        raise Exception(f'\n\n{func.__name__} **Kargs-| {x[0]} != {x[2].__name__}\n')
 
             if Reg:
-                start = time.time()
-                func_Time = func(*Items, **Dicts)
-                print('Time: {} секунд.'.format(time.time()-start))
-                print('-----------------------------------')
+                func_Time = statistics(1)
                 return func_Time
 
             return func(*Items, **Dicts)
